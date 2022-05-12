3444d41cf38afa957f4b5f0dfd4e9d2a4c447e6b
Alex Lane <97alane@gmail.com>
2022-05-12 09:55:47 -0500

    added license badges

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,13 +1,34 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
+    let licenseType = license; 
+  if (licenseType === 'MIT') {
+    yourLicense = `![License: MIT](https://img.shields.io/badge/License-MIT-greens.svg)`
+  } else if (licenseType === 'GPLv3') {
+    yourLicense = `![Apache2.0 license](https://img.shields.io/badge/License-Apache2.0-green.svg)`
+  } else if (licenseType === 'GPL') {
+    yourLicense = `![GNU Public License](https://img.shields.io/badge/License-GNUPublicv3.0-green.svg)`
+  } else {
+    license.license = "N/A"
+  }
+  return yourLicense;
   
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  
+    let licenseLinks; 
+    if (license === 'MIT') {
+      licenseLinks = `[License: MIT](https://choosealicense.com/licenses/mit/)`
+    } else if (license === 'GPLv3') {
+      licenseLinks = `[Apache2.0 license](https://choosealicense.com/licenses/apache-2.0/)`
+    } else if (license === 'GPL') {
+      licenseLinks = `[GNU Public License](https://choosealicense.com/licenses/gpl-3.0/)`
+    } else {
+      license.license = "N/A"
+    }
+    return licenseLinks;
 
 }
 
